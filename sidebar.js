bb5de54a6dd4b96452668045616f8e69d9ad07ac
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2022-02-10 02:32:53 +0530

    Menu added

---
+++ sidebar.js
@@ -0,0 +1,47 @@
+const sidebar = document.querySelector(".sidebar");
+const menuIcon = document.querySelector(".vertical-menu-icon-container");
+const menuIconBar1 = document.querySelector(".bar-1");
+const menuIconBar2 = document.querySelector(".bar-2");
+const menuIconBar3 = document.querySelector(".bar-3");
+const sidebarMenu = document.querySelector(".sidebar-sections-container");
+const sidebarMenuSpan = document.querySelectorAll(".sidebar-sections-span");
+const sidebarMenuLi = document.querySelectorAll(".sidebar-sections-li");
+const section = document.querySelectorAll(".section");
+const codechefImg = document.querySelector(".sidebarImg");
+const verticalMenu = document.querySelector(".vertical-menu-icon-container");
+const socials = document.querySelector(".socials");
+// For open/close of sidebar menu
+
+menuIcon.addEventListener("click", (e) => {
+    menuIconBar1.classList.toggle("bar-1-change");
+    menuIconBar2.classList.toggle("bar-2-change");
+    menuIconBar3.classList.toggle("bar-3-change");
+    sidebar.classList.toggle("sidebar-change");
+    // codechefImg.classList.toggle("hide-it");
+    sidebarMenu.classList.toggle("sidebar-sections-container-change");
+    verticalMenu.classList.toggle("vertical-menu-icon-container-change");
+});
+
+// For hover effect on extended sidebar menu options
+
+sidebarMenuSpan.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+        item.classList.toggle("sidebar-sections-span-change");
+    });
+    item.addEventListener("mouseleave", (e) => {
+        item.classList.toggle("sidebar-sections-span-change");
+    });
+    item.addEventListener("click", (e) => {
+        menuIconBar1.classList.toggle("bar-1-change");
+        menuIconBar2.classList.toggle("bar-2-change");
+        menuIconBar3.classList.toggle("bar-3-change");
+        sidebar.classList.toggle("sidebar-change");
+        setTimeout(() => {
+            sidebarMenu.classList.toggle("sidebar-sections-container-change");
+
+            verticalMenu.classList.toggle(
+                "vertical-menu-icon-container-change"
+            );
+        }, 300);
+    });
+});
