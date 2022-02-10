d0a82d1ee9db1c5fb93fe793d8af1c986a006df9
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2022-02-10 17:31:51 +0530

    Fix bugs

--- sidebar.js
+++ sidebar.js
@@ -10,6 +10,7 @@ const section = document.querySelectorAll(".section");
 const codechefImg = document.querySelector(".sidebarImg");
 const verticalMenu = document.querySelector(".vertical-menu-icon-container");
 const socials = document.querySelector(".socials");
+
 // For open/close of sidebar menu
 
 menuIcon.addEventListener("click", (e) => {
@@ -22,6 +23,36 @@ menuIcon.addEventListener("click", (e) => {
     verticalMenu.classList.toggle("vertical-menu-icon-container-change");
 });
 
+// Close the menu on click on screen
+
+section.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        let screenheight = (35 / 100) * window.screen.height;
+        if (e.clientY > screenheight && e.clientY > 100) {
+            menuIconBar2.classList.remove("bar-2-change");
+            menuIconBar3.classList.remove("bar-3-change");
+            menuIconBar1.classList.remove("bar-1-change");
+            sidebar.classList.remove("sidebar-change");
+            setTimeout(() => {
+                sidebarMenu.classList.remove(
+                    "sidebar-sections-container-change"
+                );
+            }, 300);
+        }
+    });
+});
+
+// Close the menu omn scroll
+
+document.addEventListener("scroll", (e) => {
+    menuIconBar2.classList.remove("bar-2-change");
+    menuIconBar3.classList.remove("bar-3-change");
+    menuIconBar1.classList.remove("bar-1-change");
+    sidebar.classList.remove("sidebar-change");
+    setTimeout(() => {
+        sidebarMenu.classList.remove("sidebar-sections-container-change");
+    }, 300);
+});
 // For hover effect on extended sidebar menu options
 
 sidebarMenuSpan.forEach((item) => {
